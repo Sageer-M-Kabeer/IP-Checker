f3fd4c7a63aa925416630af8aaba099921a3fae6
Hendrix <78979577+Sageer-M-Kabeer@users.noreply.github.com>
2021-07-25 20:09:32 +0100

    Update ip_checker.py

--- ip_checker.py
+++ ip_checker.py
@@ -1,13 +1,13 @@
 import socket
 
-print('''
+print("\033[1;32;40m"+'''
   ___ ___    ___ _  _ ___ ___ _  _____ ___ 
  |_ _| _ \  / __| || | __/ __| |/ / __| _ \
   | ||  _/ | (__| __ | _| (__| ' <| _||   /
  |___|_|    \___|_||_|___\___|_|\_\___|_|_\
                                            
 ''')
-print('By SageerMK aka Hendri3x22')
+print("\033[1;32;40m"+'By SageerMK aka Hendri3x22')
 
 hostnamez= socket.gethostname()
 ipad=socket.gethostbyname(hostnamez)
